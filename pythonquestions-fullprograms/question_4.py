908d8117614061f0aa654bc194c9bb89bee366bf
jamesli1111 <jli24@ocdsb.ca>
2020-11-27 03:28:45 -0800

    On question 5

---
+++ pythonquestions-fullprograms/question_4.py
@@ -0,0 +1,16 @@
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+word1 = word1.lower()
+word2 = word2.lower()
+
+if word1 > word2:
+    print(f"The second word, {word2}, is alphabetically first")
+else:
+    print(f"The first word, {word1}, is alphabetically first")
+
+# Upper case letters are actually smaller than lower case letters, meaning they have a higher priority than lower case letters.  
+# The code will recognize ANY uppercase letter as smaller than ANY lowercase number. 
+# So for example, uppercase letter 'X' is still smaller than lowercase letter "a" and is thus still alphabetically first according to our cold.
+# By turning all letters into lowercase letters, all letters can now actually be evaluated by their places int he alphabet.
+# The code will now be able to recognize which number is alphabetically first, without uppercase letters interfering
