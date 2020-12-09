d9c55dc9d9e9564429058a272b72e5dff7a63001
jamesli1111 <jli24@ocdsb.ca>
2020-12-08 17:38:25 -0800

    Edited question 15 to be more efficient

--- pythonquestions-fullprograms/question_15.py
+++ pythonquestions-fullprograms/question_15.py
@@ -1,19 +1,13 @@
-class MarkTooLowError(Exception):
-    pass
-class MarkTooHighError(Exception):
+class MarkRangeError(Exception):
     pass
 while True:  
     try:
         percentage_mark= float(input("Enter a percentage mark: "))
-        if percentage_mark<0:
-            raise MarkTooLowError
-        elif percentage_mark>100:
-            raise MarkTooHighError
+        if percentage_mark<0 or percentage_mark>100:
+            raise MarkRangeError
         break
     except ValueError:
         print(f"Enter a number")
-    except MarkTooHighError:
-        print("Mark cannot be greater than 100")
-    except MarkTooLowError:
-        print("Mark cannot be negative")        
+    except MarkRangeError:
+        print("Mark is not within possible range")      
 print("Thank you!")
