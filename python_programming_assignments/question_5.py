12583841c6e33b8197f7fb5c42a59b23c457f70a
jamesli1111 <jli24@ocdsb.ca>
2020-12-11 12:02:26 -0800

    Finished guessing game part for bounds between 1 to 100

---
+++ python_programming_assignments/question_5.py
@@ -0,0 +1,16 @@
+import random
+count = 0
+game_type = input (f"Do you want to:\n1. Choose a number between 1 and 100 or\n2. Choose the bounds for a random number?\n")
+
+if game_type == "1":
+     
+    while True:
+            generated_number = random.randint(1,4)
+            guess = int(input("What is your guess?"))
+            count += 1
+
+            if guess == generated_number:
+                print (f"Congratulations, you got the correct number in {count} guesse(s)")
+                break
+        
+
--- question_6(original).py
+++ question_6(original).py
@@ -14,10 +14,8 @@ class PlayAgainInputError(Exception):
 
     pass
 
-#this function asks for the user's values of their quadratic function, then makes a table of values
 def quadratic():
 
-    #Makes sure the user inputs a number for a, b, and c values
     while True:
         try:
             print("For the Quadratic function: y=ax^2 + bx + c")
@@ -28,76 +26,54 @@ def quadratic():
         except ValueError:
             print("That is not a number!")
 
-    #makes sure that the table of values begins with negative domain values
     x = (-b/(2*a)) - 6
+    print(f"{'x':>8}{'y':>9}")
 
-    print(f"{'x':>11}{'y':>9}")
-
-    #rounds values of a quadratic function to one decimal place and formats numbers to the right by 12 and 9 spaces
-
-    #formats the negative domain part of the table of values
-    for i in range(6):
+    for i in range(11):
         x += 1
-        print(f"{round(x,2):>12}{round(a*x**2+b*x+c,2):>9}")
+        print(f"{round(x,2):>10}{round(a*x**2+b*x+c,2):>9}")
 
-    #formats positive domain part of the table of values
-    for i in range(5):
-        x += 1
-        print(f"{round(x,2):>12}{round(a*x**2+b*x+c,2):>9}")
-
-#this function asks for the values of the user's linear relation, then makes a table of values 
 def linear():
 
-    #Makes sure the user inputs a number for a, b, and c values
     while True:
         try:
-            print("For the linear equation: y = mx + b")
-            m = float(input("Enter a number value for 'm': "))
+            print("For the linear equation: 0 =ax + by + c")
+            a = float(input("Enter a number value for 'a': "))
             b = float(input("Enter a number value for 'b': "))
+            c = float(input("Enter a number value for 'c': "))
             break
         except ValueError:
             print("That is not a number!")
 
-    #makes sure that the table of values begins with negative domain values   
-    x = -6
-
-    print(f"{'x':>11}{'y':>9}")
-
-    #Does the same thing as thing as code lines, 38 through 46
-
-    #formats negative domain part of table
-    for i in range(5):
-        x += 1
-        print(f"{round(x,2):>12}{round(m*x+b,2):>9}")
-
-    #formats positive domain part of table 
-    for i in range(6):
-        x += 1
-        print(f"{round(x,2):>12}{round(m*x+b,2):>9}")
+    print(f"{'x':>8}{'y':>9}")
+    
+    for x in range(-5,6):
+        y = - (a*x + c) / b
+        if y == -0:
+            y == 0
+        print(f"{round(x,2):>8}{round(y,2):>10}")
 
 while True:
     while True: 
         try:
             quadratic_or_linear = input("Which relationship would you like to see a table of values of? \n1. quadratic \n2. linear \n")
 
-            #makes sure that the user enters either "1" or "2"
             if quadratic_or_linear != "1" and quadratic_or_linear != "2":
                 raise QuadraticOrLinearInputError
             break
         
         except QuadraticOrLinearInputError:
             print("Please enter 1 for a quadratic function and 2 for a linear relation")
-    #the number '1' represents the user picking a quadratic function
+
     if quadratic_or_linear == "1":
         quadratic()
-    #the number '2' represents the user picking a linear relation
     elif quadratic_or_linear == "2":
         linear()
+
     while True:
         try:   
             play_again = input("Would you like to see another table of values of another relation? (Y/N) ")
 
-            #makes sure that the user types either "Y" or "N"
             if play_again != "N" and play_again != "Y":
                 raise PlayAgainInputError
             break
